e7a8040da1b257055e2823eca5803d32ca149153
danielgard1 <danielgardner1357@gmail.com>
2021-04-06 11:21:38 -0600

    Static Exercise Complete

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var celcius = TempConverter.FahrenheitToCelsius(68);
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(20);
+
+            Console.WriteLine("After Conversion:");
+            Console.WriteLine($"Celcius {celcius}");
+            Console.WriteLine($"Fahrenheit {fahrenheit}");
+        }
+    }
+}
